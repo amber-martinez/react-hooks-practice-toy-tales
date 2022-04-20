b488c5e25e26bc06851a90a91fa0258392069ed7
Amber <martinez.amber1028@gmail.com>
2022-04-19 18:43:04 -0700

    commit tree 4e262666ac300e18e0341fa43d55d201787f5568

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,42 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, deleteToy, updateLikes }) {
+
+  const { name, image, likes, id } = toy;
+
+  function handleDeleteToy() {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE"
+    })
+    .then((r) => r.json())
+    .then(() => deleteToy(toy))
+  }
+
+  function handleLikeBtn() {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "likes": likes + 1
+      })
+    })
+    .then(r => r.json())
+    .then((updatedToyLike) => updateLikes(updatedToyLike))
+  }
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{likes} Likes </p>
+      <button className="like-btn" onClick={handleLikeBtn}>Like {"<3"}</button>
+      <button className="del-btn" onClick={handleDeleteToy}>Donate to GoodWill</button>
     </div>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toys, deleteToy, updateLikes }) {
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {toys.map((toy) => (
+        <ToyCard
+        toy={toy}
+        key={toy.id}
+        id={toy.id}
+        name={toy.name}
+        image={toy.image}
+        likes={toy.likes}
+        deleteToy={deleteToy}
+        updateLikes={updateLikes}
+        />
+      ))}
+    </div>
   );
 }
 
